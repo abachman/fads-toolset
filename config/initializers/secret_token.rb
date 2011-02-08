06ddbfdaa3064d290aa7798ef7ecead615b89357
Adam Bachman <adam.bachman@gmail.com>
2011-02-08 13:46:16 -0500

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FadsTools::Application.config.secret_token = '212e682dd2b7e27b57c990e49c9c417918202e5c27b984d79f537c5550705a231b0ef4a11189cf8b0b8b5671bd8debfa394199e5a9c902b29734a9e5f59e695e'
